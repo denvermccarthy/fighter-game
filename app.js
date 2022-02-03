cbb6161516f42d87167f59d7a3df7ed0b3a6c380
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-02 21:16:40 -0800

    got goblin hp to stay at zero and image change on death, user hp does not decrease when dead goblin is clicked.

--- app.js
+++ app.js
@@ -16,29 +16,30 @@ let defeatCount = 0;
 let userHp = 10;
 let goblins = [
     {
-        id: 1,
+        id: 0,
         name: 'bart',
         hp: 2,
-        img: undefined
+        img: './assets/icons8-goblin-64.png'
     }, {
-        id: 2,
+        id: 1,
         name: 'jerry',
         hp: 1,
-        img: undefined
+        img: './assets/icons8-goblin-64.png'
     }
 ];
-let goblinId = 3;
+let goblinId = 2;
 // set event listeners
 function displayGoblins() {
     gobArea.innerHTML = '';
     for (const goblin of goblins) {
         const gob = renderGoblin(goblin);
         gob.addEventListener('click', () => {
-            console.log('clicked', goblin);
+            // console.log('clicked', goblin);
             playGame(goblin);
         });
         gobArea.append(gob);
     }
+
 }
 displayGoblins();
 form.addEventListener('submit', (e) => {
@@ -49,7 +50,7 @@ form.addEventListener('submit', (e) => {
         id: goblinId++,
         name: formData.get('gob-name'),
         hp: Math.ceil(Math.random() * 4),
-        img: undefined
+        img: './assets/icons8-goblin-64.png'
     };
     
     goblins.push(goblin);
@@ -59,32 +60,60 @@ form.addEventListener('submit', (e) => {
   
 });
 
-function hitUser() {
-    // console.log('user was hit');
+function hitUser(g) {
+    console.log('user was hit');
     userHp--;
+    if (g.hp === 0){
+        userHp++;
+    }
     hpEl.textContent = userHp;
 }
 
 function hitGoblin(goblin) {
 //  console.log('goblin was hit')
     goblin.hp--;
-    displayGoblins();
+    
     if (goblin.hp === 0) {
         defeatCount++;
         defeatEl.textContent = defeatCount; 
-    }
+        goblin.img = './assets/dead.png';
+    } else if (goblin.hp < 0) {
+        goblin.hp = 0;
+    } 
+    displayGoblins();
 }
 
 function playGame(goblin) {
     
+
     Math.random() < .75 ? hitGoblin(goblin) : console.log('user miss');
 
-    Math.random() < .50 ? hitUser() : console.log('goblin miss');
+    Math.random() < .50 ? hitUser(goblin) : console.log('goblin miss');
 
-    
+    // if (goblin.hp === 0) {
+    //     goblin.img = './assets/dead.png';
+    //     console.log('dead', goblin);
+    //     displayGoblins();
+    // } else if (goblin.hp < 0) {
+    //     // set to zero, find a way to keep at zero
+    //     goblin.hp = 0;
+    //     displayGoblins();
+    // } 
 
 }
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
   // goblin.hp.every(equalsZero) ? console.log('win') : console.log('loss');
+
+   // const isDead = goblins.forEach((goblin) => {
+    //     if (goblin.hp < 0) {
+
+    // });
+
+    // if (isDead) {
+    //     // remove all goblins 
+    // }
+reset.addEventListener('click', () => {
+    window.location.reload();
+});  
